fb2649e362692cd0df7472b4ed16d0da727db69f
Mohammad Zahed <mozex.gm@gmail.com>
2023-06-29 19:39:53 +0300

    add flush all command

--- src/Commands/FlushAllCommand.php
+++ src/Commands/FlushAllCommand.php
@@ -3,18 +3,48 @@
 namespace Mozex\ScoutBulkActions\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Console\ConfirmableTrait;
 use Mozex\ScoutBulkActions\Commands\Concerns\FindsSearchableModels;
 
 class FlushAllCommand extends Command
 {
     use FindsSearchableModels;
+    use ConfirmableTrait;
 
-    public $signature = 'scout:flush-all';
+    public $signature = 'scout:flush-all {--force : Force the operation to run when in production}';
 
     public $description = 'Flush the records of all models from the index.';
 
     public function handle(): int
     {
+        if (! $this->confirmToProceed()) {
+            return self::FAILURE;
+        }
+
+        $this->info('Flushing started.');
+
+        $models = $this->getSearchableModels();
+
+        $bar = $this->output->createProgressBar($models->count());
+
+        foreach ($models as $model) {
+            // callSilently returns 0 on success and 1 on failure
+            if ($this->callSilently('scout:flush', ['model' => $model])) {
+                $this->newLine();
+                $this->error(sprintf('Flushing [%s] has been failed.', $model));
+
+                if (! $this->confirm('Do you want to continue?')) {
+                    $this->newLine();
+
+                    return self::FAILURE;
+                }
+            }
+
+            $bar->advance();
+        }
+
+        $this->newLine();
+        $this->info('Flushing finished successfully.');
 
         return self::SUCCESS;
     }
