493e68802cc41d4f1fb63a5fac9b76ef66783b95
Mohammad Zahed <mozex.gm@gmail.com>
2023-06-29 19:40:47 +0300

    add refresh command

--- src/Commands/RefreshCommand.php
+++ src/Commands/RefreshCommand.php
@@ -3,20 +3,62 @@
 namespace Mozex\ScoutBulkActions\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Console\ConfirmableTrait;
 use Mozex\ScoutBulkActions\Commands\Concerns\FindsSearchableModels;
 
 class RefreshCommand extends Command
 {
     use FindsSearchableModels;
+    use ConfirmableTrait;
 
     public $signature = 'scout:refresh
-            {model : Class name of model to bulk import}
-            {--c|chunk= : The number of records to import at a time (Defaults to configuration value: `scout.chunk.searchable`)}';
+            {model? : Class name of model to bulk import}
+            {--c|chunk= : The number of records to import at a time (Defaults to configuration value: `scout.chunk.searchable`)}
+            {--force : Force the operation to run when in production}';
 
-    public $description = 'Import all models into the search index.';
+    public $description = 'Flush models then Import them into the search index.';
 
     public function handle(): int
     {
+        if (! $this->confirmToProceed()) {
+            return self::FAILURE;
+        }
+
+        $model = $this->argument('model');
+
+        return $model
+            ? $this->handleModel($model)
+            : $this->handleAll();
+    }
+
+    private function handleModel(string $model): int
+    {
+        $flushOutput = $this->call('scout:flush', ['model' => $model]);
+
+        if ($flushOutput !== self::SUCCESS) {
+            return $flushOutput;
+        }
+
+        $this->call('scout:import', array_filter([
+            'model' => $model,
+            '--chunk' => $this->option('chunk'),
+        ]));
+
+        return self::SUCCESS;
+    }
+
+    protected function handleAll(): int
+    {
+        $flushOutput = $this->call('scout:flush-all', ['--force' => true]);
+
+        if ($flushOutput !== self::SUCCESS) {
+            return $flushOutput;
+        }
+
+        $this->call('scout:import-all', array_filter([
+            '--force' => true,
+            '--chunk' => $this->option('chunk'),
+        ]));
 
         return self::SUCCESS;
     }
